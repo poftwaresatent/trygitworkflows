482432f8d7d0a60b22ff12c75bb8817cba3ebc86
Roland Philippsen <poftwaresatent@gmail.com>
2022-08-22 09:32:17 +0200

    fix the test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import unittest
 
 class SomeTest(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(3, 2)
+        self.assertEqual(3, 3)
 
 if __name__ == "__main__":
     unittest.main()
