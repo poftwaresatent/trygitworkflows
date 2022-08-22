9694331fc0570dda68793abf87a370585f86995c
Roland Philippsen <poftwaresatent@gmail.com>
2022-08-22 10:07:36 +0200

    rtfm...ing...

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import unittest
 
 class SomeTest(unittest.TestCase):
     def test_something(self):
+        # reading up on protected branches ...
+        # https://docs.github.com/en/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests
         self.assertEqual(3, 44)
 
 if __name__ == "__main__":
