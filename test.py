253160c8d6becdc4e57e6ff8734f84331865a401
Roland Philippsen <poftwaresatent@gmail.com>
2022-08-22 01:18:22 -0700

    rtfm...ing... (#2)
    seems that this works as a charm now -- need to dig through the repo settings page to find these goodies

--- test.py
+++ test.py
@@ -2,7 +2,9 @@ import unittest
 
 class SomeTest(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(3, 44)
+        # reading up on protected branches ...
+        # https://docs.github.com/en/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests
+        self.assertEqual(2, 2)
 
 if __name__ == "__main__":
     unittest.main()
