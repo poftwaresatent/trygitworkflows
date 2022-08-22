fc29be90634f449bf9dd45bc0982099e6a15bbd3
Roland Philippsen <poftwaresatent@gmail.com>
2022-08-22 10:12:43 +0200

    ok reuired approval works... how about making the test pass?

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ class SomeTest(unittest.TestCase):
     def test_something(self):
         # reading up on protected branches ...
         # https://docs.github.com/en/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests
-        self.assertEqual(3, 44)
+        self.assertEqual(2, 2)
 
 if __name__ == "__main__":
     unittest.main()
